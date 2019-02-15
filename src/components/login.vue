793e89597372a08f463446f356a3b95f28f509eb
sanmen <1217170310@qq.com>
2019-02-15 15:43:44 +0800

    登录-表单组件引入

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,15 +1,31 @@
 <template>
-    <div>
-        login--
-    </div>
+<div class="login-wrap">
+    <h2>用户登录</h2>
+    <el-form class="login-form" :label-position="labelPosition" label-width="80px" :model="formdata">
+        <el-form-item label="用户名">
+            <el-input v-model="formdata.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码">
+            <el-input v-model="formdata.password"></el-input>
+        </el-form-item>
+        <el-button class="login-btn" type="success">登录</el-button>
+    </el-form>
+</div>
 </template>
 
 <script>
 export default {
-
-}
+    data() {
+      return {
+        labelPosition: 'top',
+        formdata: {
+          usrename: '',
+          password: ''
+        }
+      }
+    }
+};
 </script>
 
 <style>
-
 </style>
