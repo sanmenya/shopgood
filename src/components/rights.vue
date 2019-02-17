f534563a79c9e30d3368546d27cc3bae1d1b1402
sanmen <1217170310@qq.com>
2019-02-17 16:52:52 +0800

    权限列表

--- src/components/rights.vue
+++ src/components/rights.vue
@@ -2,12 +2,44 @@
     <el-card class="box">
         <!-- 面包屑 -->
         <cus-bread level1="权限管理" level2="权限列表"></cus-bread>
+        <!-- 表格 -->
+        <el-table :data="list" style="width: 100%" height="700px">
+      <el-table-column type="index" label="#" width="230"></el-table-column>
+      <el-table-column prop="authName" label="权限名称" width="260"></el-table-column>
+      <el-table-column prop="path" label="路径" width="330"></el-table-column>
+      <el-table-column prop="mobile" label="层级" width="350">
+          <template slot-scope="list">
+              <span v-if="list.row.level==='0'">一级</span>
+              <span v-if="list.row.level==='1'">二级</span>
+              <span v-if="list.row.level==='2'">三级</span>
+          </template>
+      </el-table-column>
+    </el-table>
     </el-card>
 </template>
 
 <script>
 export default {
-
+    data(){
+        return  {
+            list:[]
+        }
+    },
+    created(){
+        this.getTable()
+    },
+    methods:{
+        async getTable(){
+            const AUTH_TOKEN = localStorage.getItem('token')
+            this.$http.defaults.headers.common['Authorization'] = AUTH_TOKEN
+            const res = await this.$http.get('rights/list')
+            console.log(res)
+            const {data,meta:{status}} = res.data
+            if(status === 200) {
+                this.list = data
+            }
+        }
+    }
 }
 </script>
 
